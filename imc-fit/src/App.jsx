2a4d7531e20926ee51535157f9ee2d2c5dc7a914
Paulo Henrique <ph55@hotmail.com>
2023-07-26 19:59:03 -0300

    Update App.jsx

--- imc-fit/src/App.jsx
+++ imc-fit/src/App.jsx
@@ -1,9 +1,15 @@
+import Form from './components/Form/Form'
 import Header from './components/Header/Header'
+import MainImg from './components/MainImg/MainImg'
 
 function App() {
   return (
     <>
       <Header/>
+      <main>
+        <MainImg/>
+        <Form/>
+      </main>
     </>
   )
 }
