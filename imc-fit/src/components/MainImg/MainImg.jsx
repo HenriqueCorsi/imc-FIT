0eea7c2bb2f90bab4ac5e1306ce0d352aa0af8f9
Paulo Henrique <ph55@hotmail.com>
2023-07-26 19:59:28 -0300

    Create MainImg.jsx

---
+++ imc-fit/src/components/MainImg/MainImg.jsx
@@ -0,0 +1,11 @@
+import mainImg from '../../assets/img/main-img.jpg'
+
+function MainImg(){
+    return(
+        <>
+            <img className='main-img' src={mainImg} alt="" />
+        </>
+    )
+}
+
+export default MainImg;
