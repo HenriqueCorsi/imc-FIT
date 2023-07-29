a81702d198b72db989b33b49c57adac012c9dede
Paulo Henrique <ph55@hotmail.com>
2023-07-28 21:11:24 -0300

    Update Form.jsx

--- imc-fit/src/components/Form/Form.jsx
+++ imc-fit/src/components/Form/Form.jsx
@@ -2,6 +2,14 @@ import { useState } from 'react';
 import '../../assets/css/form-style.css'
 
 function Form(){
+
+    const [peso, setPeso] = useState();
+    const [altura, setAltura] = useState();
+
+    function calculaImc(){
+       
+    }
+
     return(
         <>
             <form className='form'>
@@ -11,14 +19,29 @@ function Form(){
                 <hr />
                 <div className='form-box'>
                    <label>Peso</label>
-                   <input type="number" id="weight" step="0.1" min="0" required placeholder='kg'/>
+                   <input 
+                    type="number"  
+                    id="weight" 
+                    step="0.1" 
+                    min="0" 
+                    required 
+                    placeholder='kg' 
+                    onChange={(e) => setPeso(e.target.value)}/>
                 </div>
                 <div className='form-box'>
                     <label>Altura</label>
-                    <input type="number" id="height" step="0.01" min="0" required placeholder='cm'/>
+                    <input 
+                        type="number" 
+                        id="height" 
+                        step="0.01" 
+                        min="0" 
+                        required 
+                        placeholder='cm'
+                        onChange={(e) => setAltura(e.target.value)}
+                        />
                 </div>
                 <div className='form-box'>
-                    <button>CALCULAR</button>
+                    <button onClick={calculaImc}>CALCULAR</button>
                 </div>
             </form>
         </>
