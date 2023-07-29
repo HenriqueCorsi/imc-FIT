6f9ce20c40c4fae700c07bf23cda6bd305436e37
Paulo Henrique <ph55@hotmail.com>
2023-07-28 21:11:34 -0300

    Create ImcTable.jsx

---
+++ imc-fit/src/components/ImcTable/ImcTable.jsx
@@ -0,0 +1,43 @@
+import '../../assets/css/imcTable-style.css';
+
+function ImcTable(){
+    return(
+        <div className='imc-table'>
+            <h3>Interpretação do IMC</h3>
+            <table>
+                <tr>
+                    <td>IMC</td>
+                    <td>Classificação</td>
+                    <td>Obesidade (GRAU)</td>
+                </tr>
+                <tr>
+                    <td>Menor que 18.5</td>
+                    <td>Magreza</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>Entre 18.6 e 24.9</td>
+                    <td>Normal</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>Entre 25.0 e 29.9</td>
+                    <td>Sobrepeso</td>
+                    <td>I</td>
+                </tr>
+                <tr>
+                    <td>Entre 30.0 e 39.9</td>
+                    <td>Obesidade</td>
+                    <td>II</td>
+                </tr>
+                <tr>
+                    <td>Maior que 40.0</td>
+                    <td>Obesidade</td>
+                    <td>III</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+export default ImcTable;
