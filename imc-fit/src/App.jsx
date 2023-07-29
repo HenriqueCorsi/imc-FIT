a53998ea81bb542d4c589beedc893a030265ed41
Paulo Henrique <ph55@hotmail.com>
2023-07-29 15:36:39 -0300

    Update App.jsx

--- imc-fit/src/App.jsx
+++ imc-fit/src/App.jsx
@@ -1,17 +1,19 @@
 import Form from './components/Form/Form'
 import Header from './components/Header/Header'
 import img from './assets/img/main-img.jpg'
-import ImcTable from './components/ImcTable/ImcTable';
 
 function App() {
   return (
     <>
       <Header/>
-      <div className='main-box'>
-        <img src={img} alt="" />
-        <Form/>
+      <div className='main-container'>
+        <div className='main-img-box'>
+          <img src={img} alt="" />
+        </div>
+        <div className='main-form-box'>
+          <Form/>
+        </div>
       </div>
-      <ImcTable/>
     </>
   )
 }
