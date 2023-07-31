084976a124634f33fb6ee6f250ce8bb274511992
Paulo Henrique <ph55@hotmail.com>
2023-07-31 17:32:13 -0300

    Update App.jsx

--- imc-fit/src/App.jsx
+++ imc-fit/src/App.jsx
@@ -1,7 +1,6 @@
 import Form from './components/Form/Form'
 import Header from './components/Header/Header'
 import img from './assets/img/main-img.jpg'
-import ImcTable from './components/ImcTable/ImcTable';
 import Footer from './components/Footer/Footer';
 
 function App() {
