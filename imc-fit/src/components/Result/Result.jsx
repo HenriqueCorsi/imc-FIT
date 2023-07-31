eb3332fca88099bde5882a214aa8ab9e1fbe18d1
Paulo Henrique <ph55@hotmail.com>
2023-07-31 15:37:16 -0300

    Update Result.jsx

--- imc-fit/src/components/Result/Result.jsx
+++ imc-fit/src/components/Result/Result.jsx
@@ -3,6 +3,7 @@ import '../../assets/css/result-style.css';
 function Result({msg, imc}){
     return(
         <>
+            <hr />
             <div className="result-container">
                 <p className='msg'>{msg}</p>
                 <p className='imc-result'>{imc}</p>
